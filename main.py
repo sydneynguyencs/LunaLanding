73e29000a67f034f6962475864b15054b3bfaf21
Sydney Nguyen <sydneynguyen@Sydneys-MacBook-Air.local>
2023-01-11 08:37:29 +0100

    Implement main

---
+++ main.py
@@ -0,0 +1,25 @@
+import gym
+
+
+## ===== ===== ===== ===== ===== ===== ===== =====
+## Main function
+## ===== ===== ===== ===== ===== ===== ===== =====
+
+
+def main():
+    env = gym.make("LunarLander-v2", render_mode="human")
+    env.action_space.seed(42)
+
+    observation, info = env.reset(seed=42)
+
+    for _ in range(1000):
+        observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
